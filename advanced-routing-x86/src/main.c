369a6dc78d6b7ac1e8a5c601019a707eaf1f6c06
giorgiodaniele15 <giorgiodaniele15@gmail.com>
2024-03-13 15:33:08 +0100

    Caricamento parte iniziale accodamento pacchetti x86

---
+++ advanced-routing-x86/src/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <net/if.h>
+#include <linux/pkt_cls.h>
+#include <linux/if_link.h>
+
+#include "options.h"
+#include "skeleton.h"
+
+#define CMD_SIZE 100
+#define MAP_PATH 1500
+
+
+int main (int argc, char **argv)
+{	
+	printf("Hello world\n");
+	return 0;
+}
+
---
+++ advanced-routing-x86/src/options.h
@@ -0,0 +1,10 @@
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+#define TIME_BASED_MODE   0
+#define STATISTICS_MODE   0
+#define TC_VERBOSE   0
+#define XDP_VERBOSE  0
+#define TESTING_MODE 1
+
+#endif
