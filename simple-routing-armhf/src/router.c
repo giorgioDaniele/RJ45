337993e16be04a9435209c0ad49301aada066b79
giorgiodaniele <giorgiodaniele15@gmail.com>
2024-03-18 11:32:44 +0100

    Rifacimento del codice, armh e x86

--- simple-routing-armhf/src/router.c
+++ simple-routing-armhf/src/router.c
@@ -1,37 +1,40 @@
 #include "vmlinux.h"
-#include "options.h"
 #include "bpf_helpers.h"
 #include "bpf_endian.h"
 
-#if TC_VERBOSE || XDP_VERBOSE
-#define DBG(fmt, ...)   \
-({						\
-	char ____fmt[] = "rj45: " fmt;  \
-	bpf_trace_printk(____fmt, sizeof(____fmt), \
-			 ##__VA_ARGS__); \
-})
-#else
-#endif
-
-/**
- * Definzione delle azioni
- * nel programma TC
-*/
+#define TC_VERBOSE  0
+#define XDP_VERBOSE 0
 
 #define ETH_ALEN 	6
 #define TC_ACT_OK 	0
-#define TC_ACT_SHOT	1
 
+/**
+ * Primitiva per stampare in /sys/kernel/debug/tracing/trace_pipe
+ * gli eventi verificatesi durante l'esecuzione dei programmi eBPF
+ * di cui sotto
+*/
+#define DBG(fmt, ...)  \
+({			  \
+	char ____fmt[] = "[RJ45]: " fmt;  \
+	bpf_trace_printk(____fmt, \
+	sizeof(____fmt), ##__VA_ARGS__); \
+})
+
+/**
+ * Definzione del valore utilizzato in associazione con
+ * la chiave all'interno della tabellina di instradamento
+*/
 struct _route 
 {
 	__u8 smac[ETH_ALEN];
 	__u8 dmac[ETH_ALEN];
-	__u8 iface;
-#if TIME_BASED_MODE
-	__u64 time;
-#endif
+	__u8 interface;
 };
 
+/**
+ * Definzione della chiave utilizzata per accedere alla
+ * tabellina di instradamento
+*/
 struct _session 
 {
 	__u32 srcip;
@@ -41,6 +44,11 @@ struct _session
 	__u8  proto;
 };
 
+/**
+ * Definzione della mappa (variabile globale) utilizzata
+ * come tabellina di instradamento attraverso una mappa
+ * eBPF
+*/
 struct 
 {
 	__uint(type, 	BPF_MAP_TYPE_HASH);
@@ -51,31 +59,19 @@ struct
 
 } routing_table SEC(".maps");
 
-#if STATISTICS_MODE
-struct _statistics 
-{
-	__u64 packets;
-	__u64 bytes;
-};
-
-struct 
-{
-	__uint(type, 	BPF_MAP_TYPE_PERCPU_HASH);
-	__uint(max_entries, 1024);
-	__type(key, 	struct _session);
-	__type(value, 	struct _statistics);
-	__uint(pinning, LIBBPF_PIN_BY_NAME);
-
-} statistics_table SEC(".maps");
-#else
-#endif
-
-#define IPPROTO_TCP  6
-#define IPPROTO_UDP 17
 
 SEC("tc")
+/**
+ * Programma da caricare sul blocco TC, sezione di uscita.
+ * Il suo compito è quello di registrare all'interno della
+ * mappa di cui sopra, data una sessione TCP oppure UDP,
+ * la terna costituita da indirizzo MAC sorgente, indirizzo
+ * MAC destinazione e numero della interfaccia di rete
+ * di uscita
+*/
 int tc_program(struct __sk_buff* ctx) 
-{
+{	
+	
 	void* data     = (void*) (__u64) ctx->data;
 	void* data_end = (void*) (__u64) ctx->data_end;
 
@@ -87,22 +83,20 @@ int tc_program(struct __sk_buff* ctx)
 	struct _session	session = {};
 	struct _route	route	= {};
 
-	/* Livello Ethernet */
-
 	if (data + sizeof(struct ethhdr) > data_end)
-		goto process_drop;
+		return TC_ACT_OK;
+
 	eth  = (struct ethhdr*) data;
 
 	__builtin_memcpy(route.smac, eth->h_source, ETH_ALEN);
 	__builtin_memcpy(route.dmac, eth->h_dest, ETH_ALEN);
-	route.iface = ctx->ifindex;
+	route.interface = ctx->ifindex;
 
 	data = data + sizeof(struct ethhdr);
 
-	/* Livello IPv4 */
-
 	if (data + sizeof(struct iphdr) > data_end)
-		goto process_pass;
+		return TC_ACT_OK;
+
 	ip4  = (struct iphdr*) data;
 
 	session.srcip = ip4->saddr;
@@ -111,103 +105,95 @@ int tc_program(struct __sk_buff* ctx)
 
 	data = data + (ip4->ihl << 2);
 	
-	/** Livello TCP/UDP */
-
-	if (ip4->protocol == IPPROTO_TCP) {
+	if (ip4->protocol == IPPROTO_TCP) 
+	{
 		if (data + sizeof(struct tcphdr) > data_end)
-			goto process_drop;
+			return TC_ACT_OK;
+
 		tcp = (struct tcphdr*) data;
-		goto process_tcp;
-	} else if (ip4->protocol == IPPROTO_UDP) {
+		session.sport = tcp->source;
+		session.dport = tcp->dest;
+	} 
+	else if (ip4->protocol == IPPROTO_UDP) 
+	{
 		if (data + sizeof(struct udphdr) > data_end)
-			goto process_drop;
+			return TC_ACT_OK;
+
 		udp = (struct udphdr*) data;
-		goto process_udp;
-	} else {
-
+		session.sport = udp->source;
+		session.dport = udp->dest;
+	} 
+	else 
+	{
 		/**
-		 * Protocollo livello trasporto
-		 * non supportato
+		 * Protocollo livello trasporto non ancora supportato
 		*/
-
-		goto process_pass;
+		return TC_ACT_OK;
 	}
 
-process_tcp:
-	session.sport = tcp->source;
-	session.dport = tcp->dest;
-	goto update_routing_table;
-process_udp:
-	session.sport = udp->source;
-	session.dport = udp->dest;
-	goto update_routing_table;
-
-update_routing_table:
-#if TIME_BASED_MODE
-	route.time = bpf_ktime_get_ns();
+#ifdef TC_VERBOSE 
+	if (udp) 
+	{
+		DBG("[TC] UDP session - from %u to %u\n", 
+			__bpf_ntohs(udp->source), 
+			__bpf_ntohs(udp->dest));
+	} 
+	if (tcp) 
+	{
+		DBG("[TC] TCP session - from %u to %u\n", 
+			__bpf_ntohs(tcp->source), 
+			__bpf_ntohs(tcp->dest));
+	}
 #else
+	/**
+	 * Nessuna stampa. Scelta consigliata se non occorre svolgere
+	 * nessuna attività di ispezione del codice!
+	*/
 #endif
 
-	bpf_map_update_elem(
-		&routing_table, &session, 
-		&route, BPF_ANY);
-
-#if TC_VERBOSE
-
-		if(udp) {
-			DBG("[TC] UDP session - from %u to %u\n", 
-				__bpf_ntohs(udp->source), 
-				__bpf_ntohs(udp->dest));
-		} 
-		if(tcp) {
-			DBG("[TC] TCP session - from %u to %u\n", 
-				__bpf_ntohs(tcp->source), 
-				__bpf_ntohs(tcp->dest));
-		}
-#else
-#endif
-
-process_pass:
-  	return TC_ACT_OK;
-process_drop:
-	return TC_ACT_SHOT;
+	/**
+	 * Inserimento all'interno della tabellina di instradamento
+	 * dell'associazione chiave valore
+	*/
+	bpf_map_update_elem(&routing_table, &session, &route, BPF_ANY);
+	return TC_ACT_OK;
 }
 
 SEC("xdp")
+/**
+ * Programma da caricare sul blocco XDP, sezione di ingresso.
+ * Il suo compito è quello di interrogare la tabellina di
+ * instradamento, quella popolata dal blocco TC, e inoltrare
+ * il pacchetto di rete, sostitudendosi al codice del kernel
+ * Linux.
+*/
 int xdp_program(struct xdp_md* ctx) 
-{	
+{
 	void* data     = (void*) (__u64) ctx->data;
 	void* data_end = (void*) (__u64) ctx->data_end;
-
-	__u64 sum  = 0;
-	__u32 sze  = 0;
-	__u16 *ptr = NULL;
 
 	struct ethhdr	*eth = NULL;
 	struct iphdr	*ip4 = NULL;
 	struct tcphdr	*tcp = NULL;
 	struct udphdr	*udp = NULL;
 
+	__u64 sum  = 0;
+	__u32 sze  = 0;
+	__u16 *ptr = NULL;
+
 	struct _session	session = {};
-	struct _route	*route  = NULL;
-
-#if STATISTICS_MODE 
-	struct _statistics *statistics = NULL;
-	struct _statistics  new_statistics = {};
-#else
-#endif
-
-	/* Livello Ethernet */
+	struct _route	*route;
 
 	if (data + sizeof(struct ethhdr) > data_end)
-		goto process_drop;
+		return XDP_PASS;
+
 	eth  = (struct ethhdr*) data;
+
 	data = data + sizeof(struct ethhdr);
 
-	/* Livello IPv4 */
-
 	if (data + sizeof(struct iphdr) > data_end)
-		goto process_drop;
+		return XDP_PASS;
+
 	ip4  = (struct iphdr*) data;
 
 	session.srcip = ip4->saddr;
@@ -215,160 +201,87 @@ int xdp_program(struct xdp_md* ctx)
 	session.proto = ip4->protocol;
 
 	data = data + (ip4->ihl << 2);
+	
+	if (ip4->protocol == IPPROTO_TCP) 
+	{
+		if (data + sizeof(struct tcphdr) > data_end)
+			return XDP_PASS;
 
-	/** Livello TCP/UDP */
-
-	if (ip4->protocol == IPPROTO_TCP) {
-		if (data + sizeof(struct tcphdr) > data_end) 
-			goto process_drop;
 		tcp = (struct tcphdr*) data;
-		goto process_tcp;
-	} else if (ip4->protocol == IPPROTO_UDP) {
+		session.sport = tcp->source;
+		session.dport = tcp->dest;
+
+	} 
+	else if (ip4->protocol == IPPROTO_UDP) 
+	{
 		if (data + sizeof(struct udphdr) > data_end)
-			goto process_drop;
+			return XDP_PASS;
+
 		udp = (struct udphdr*) data;
-		goto process_udp;
-	} else {
+		session.sport = udp->source;
+		session.dport = udp->dest;
 
+	} 
+	else 
+	{
 		/**
-		 * Protocollo livello trasporto
-		 * non supportato
+		 * Protocollo livello trasporto non ancora supportato
 		*/
-
-		goto process_pass;
+		return XDP_PASS;
 	}
-
-	/* Accelerazione del traffico */
-
-process_tcp:
-	session.sport = tcp->source;
-	session.dport = tcp->dest;
-	goto query_routing_table;
-
-process_udp:
-	session.sport = udp->source;
-	session.dport = udp->dest;
-	goto query_routing_table;
-
-query_routing_table:
 
 	route = bpf_map_lookup_elem(&routing_table, &session);
 
-	if (route == NULL) {
+	if (route == NULL)
+	{
+		return XDP_PASS;
+	}
+	
+	__builtin_memcpy(eth->h_source, route->smac, ETH_ALEN);
+	__builtin_memcpy(eth->h_dest, route->dmac, ETH_ALEN);
 
-#if XDP_VERBOSE
-		if(udp) {
-			DBG("[XDP] UDP session not found - from %u to %u (XDP_PASS)\n", 
-				__bpf_ntohs(udp->source), 
-				__bpf_ntohs(udp->dest));
-		} 
-		if(tcp) {
-			DBG("[XDP] TCP session not found - from %u to %u (XDP_PASS)\n", 
-				__bpf_ntohs(tcp->source), 
-				__bpf_ntohs(tcp->dest));
-		}
+	ip4->ttl -= 1;
+	ip4->check = 0;
+
+	sum = 0;
+	sze = sizeof(struct iphdr);
+	ptr = (__u16 *)ip4;
+	while (sze > 1)
+	{
+		sum += *ptr;
+		ptr++;
+		sze -= 2;
+	}
+	if (sze == 1)
+	{
+		sum += *(__u8 *)ptr;
+	}
+	sum = (sum & 0xFFFF) + (sum >> 16);
+	ip4->check = ~sum;
+
+
+#ifdef XDP_VERBOSE
+	if (udp)
+	{
+		DBG("[XDP] UDP session found - from %u to %u (XDP_REDIRECT)\n",
+			__bpf_ntohs(udp->source),
+			__bpf_ntohs(udp->dest));
+	}
+	if (tcp)
+	{
+		DBG("[XDP] TCP session found - from %u to %u (XDP_REDIRECT)\n",
+			__bpf_ntohs(tcp->source),
+			__bpf_ntohs(tcp->dest));
+	}
 #else
+	/**
+	 * Nessuna stampa. Scelta consigliata se non occorre svolgere
+	 * nessuna attività di ispezione del codice!
+	*/
 #endif
 
-	} else {
-
-#if XDP_VERBOSE
-		if(udp) {
-			DBG("[XDP] UDP session found - from %u to %u (XDP_REDIRECT)\n", 
-				__bpf_ntohs(udp->source), 
-				__bpf_ntohs(udp->dest));
-		} 
-		if(tcp) {
-			DBG("[XDP] TCP session found - from %u to %u (XDP_REDIRECT)\n", 
-				__bpf_ntohs(tcp->source), 
-				__bpf_ntohs(tcp->dest));
-		}
-#else
-#endif	
-
-		/**
-		 * Modifica degli indirizzi 
-		 * di livello Ethernet
-		*/
-	
-		__builtin_memcpy(eth->h_source, route->smac, ETH_ALEN);
-		__builtin_memcpy(eth->h_dest, route->dmac, ETH_ALEN);
-
-		/**
-		 * Messa a punto dei paramentri
-		 * intestazione IPv4
-		*/
-	
-		ip4->ttl   -=1;
-		ip4->check = 0;
-
-		/**
-		 * Calcolo del checksum IPv4
-		*/
-
-		sum = 0;
-		sze = sizeof(struct iphdr);
-		ptr = (__u16*) ip4;
-		while (sze > 1) {
-			sum += *ptr;
-			ptr++;
-			sze -= 2;
-		}
-		if (sze == 1)
-			sum += *(__u8*) ptr;
-		sum = (sum & 0xFFFF) + (sum >> 16);
-		ip4->check = ~sum;
-
-#if STATISTICS_MODE
-
-		/**
-		 * Aggiornamento delle statistiche
-		*/
-
-		statistics = bpf_map_lookup_elem(&statistics_table, &session);
-		if (statistics) {
-
-			/**
-			 * Aggiornamento delle statistiche
-			 * finora raccolte
-			*/
-
-			statistics->packets += (__u64) 1;
-			statistics->bytes   += (__u64) (data_end - data);
-
-		} else {
-
-			/**
-			 * Aggiunta di un nuovo
-			 * elemento all'interno
-			 * della mappa
-			*/
-
-			new_statistics.packets = (__u64) 1;
-			new_statistics.bytes   = (__u64) (data_end - data);
-		
-			bpf_map_update_elem(
-				&statistics_table, &session, 
-				&new_statistics, BPF_NOEXIST);
-		}
-#else
-#endif	
-		/**
-		 * Accelerazione del pacchetto
-		 * di rete
-		*/
-
-		return bpf_redirect(route->iface, 0);
-	}
-
-process_pass:
-  	return XDP_PASS;
-process_drop:
-	return XDP_DROP;
+	return bpf_redirect(route->interface, 0);
 }
 
-/**
- * Licenza
-*/
 
 char _license [] SEC("license") = "GPL";
